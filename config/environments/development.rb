6f0beb73c3da2ae0d7ff104a8655c5ec9f028c67
woropangesti <131025767+woropangesti@users.noreply.github.com>
2024-06-13 08:38:03 +0000

    Update latest SC

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #config.hosts << “https://symmetrical-space-invention-jxqwj79qj5gf5g9x-3000.app.github.dev/”
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+data = {full_name: 'Woro Pangesti', email: 'pangestiworogmail.com', password: '1234abc'}
+user = User.create(data)
+
+user = User.first
+articles = [
+    {title: 'Title article 1', description: 'description article 1', user: user},
+    {title: 'Title article 2', description: 'description article 2', user: user},
+    {title: 'Title article 3', description: 'description article 3', user: user}
+]
+
+user = Article.create(articles)
