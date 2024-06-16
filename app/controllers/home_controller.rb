7e461d0cdd3e5770094c1afafa9cdc5f761862fb
woropangesti <131025767+woropangesti@users.noreply.github.com>
2024-06-16 02:21:22 +0000

    Selesai membuat teks area untuk komentar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,15 @@ class HomeController < ApplicationController
   def read
     @article = Article.where(id: params[:article_id]).first
   end
+
+  def save_comment
+    @article = Article.where(id: params[:article_id]).first
+    comment = @article.comments.new
+    comment.comment = params[:comment]
+    if comment.save
+      redirect_to read_path(article_id: @article.id, notice: 'Success')
+    else
+      redirect_to read_path(article_id: @article.id, notice: 'Error')
+    end
+  end
 end
