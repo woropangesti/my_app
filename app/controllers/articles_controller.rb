4b6c79f6a2530db3e9fa231ecc5d6d6e40801013
woropangesti <131025767+woropangesti@users.noreply.github.com>
2024-06-16 03:34:08 +0000

    Selesai membuat login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
+  http_basic_authenticate_with name: 'user', password: 'secret'
 
   # GET /articles or /articles.json
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  http_basic_authenticate_with name: 'user', password: 'secret'
 
   # GET /users or /users.json
   def index
