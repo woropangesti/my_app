05ee03567cb834707e5ea579090618c6669e7a7e
woropangesti <131025767+woropangesti@users.noreply.github.com>
2024-06-16 01:29:12 +0000

    Selesai membuat user, relasi, generator artikel

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-data = {full_name: 'Woro Pangesti', email: 'pangestiworogmail.com', password: '1234abc'}
+data = {full_name: 'Woro Pangesti', email: 'pangestiworo@gmail.com', password: '1234abc'}
 user = User.create(data)
 
 user = User.first
